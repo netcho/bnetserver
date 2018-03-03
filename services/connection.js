7f523d76a0755f487ae6b355c4580e93c912fa05
kaloyan <kkotlarski@abv.bg>
2018-03-03 14:49:44 +0200

    Refactored ConnectionService
    Signed-off-by: kaloyan <kkotlarski@abv.bg>

--- services/connection.js
+++ services/connection.js
@@ -1,34 +1,12 @@
 /**
  * Created by kaloyan on 8.7.2016 г..
  */
-const process = require("process");
-const Service = require("../service.js");
+'use strict';
+
+const Service = require("./service.js");
 
 module.exports = class ConnectionService extends Service{
-    constructor(socket){
-        super("ConnectionService", socket);
-        
-        this.serviceHash = 0x65446991;
-        
-        this.registerHandler("Connect", function(request, response, token, send){
-
-            response.client_id = request.client_id;
-            response.server_id.label = process.pid;
-            response.server_id.epoch = Math.floor(Date.now() / 1000);
-            response.server_time = Math.floor(Date.now());
-            response.use_bindless_rpc = request.use_bindless_rpc;
-
-            /*this.timeout = setTimeout(function(){
-
-            }, 15*1000);*/
-
-            send(token, response);
-        });
-
-        this.registerHandler("KeepAlive", function (request, response, token, send) {
-            //this.timeout.clearTimeout();
-
-            send(token, response);
-        });
+    constructor(){
+        super('proto/bnet/connection_service.proto');
     }
 };
